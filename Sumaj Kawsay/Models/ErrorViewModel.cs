79ab063f43d643db705a241a74c587b072222159
Josep129 <44200944+Josep129@users.noreply.github.com>
2020-11-20 13:08:23 -0500

    Agregar archivos de proyecto.

---
+++ Sumaj Kawsay/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sumaj_Kawsay.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
