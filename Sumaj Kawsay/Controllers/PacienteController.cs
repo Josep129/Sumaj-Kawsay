e863f1ee87b860ba5e3ea867564e08b0f294a9b6
Josep129 <44200944+Josep129@users.noreply.github.com>
2020-11-20 16:17:53 -0500

    first

---
+++ Sumaj Kawsay/Controllers/PacienteController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sumaj_Kawsay.Controllers
+{
+    public class PacienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
